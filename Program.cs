62393d3b18ab87ab7d340ac10054478083450958
Boya Pushpalatha <60311622+Pushpalatha76@users.noreply.github.com>
2021-10-20 14:17:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace Demo_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo Reflection.....!");
+
+            Type T = typeof(string);
+            Console.WriteLine("Type of Name:\t{0}", T.Name);
+            Console.WriteLine("Full na,e of the type defined :\t{0}", T.FullName);
+            Console.WriteLine("Type of Assembly in which it is defined :\t{0}", T.Assembly);
+            Console.WriteLine("Information about Base Type :\t{0}", T.BaseType);
+
+            Dynamic_Object MyObj1 = new Dynamic_Object();
+            MyObj1.First_Method();
+            MyObj1.Second_Method();
+            dynamic MyObj2 = new Dynamic_Object();
+            MyObj2.First_Method();
+            MyObj2.Second_Method();
+        }
+    }
+}
---
+++ Reflection_Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Demo_Reflection
+{
+    class Dynamic_Object
+    {
+        public Dynamic_Object() { }
+
+        public Dynamic_Object(String Msg) { }
+
+        public void First_Method() 
+        {
+            Console.WriteLine("Inside First Method");
+        }
+
+        public void Second_Method() 
+        {
+            Console.WriteLine("Inside Second Method");
+        }
+    }
+}
